37943e5bddb43e7169aa2f4911147450937b5451
praashant99 <praashant99@users.noreply.github.com>
2020-07-31 15:59:02 +0530

    Code file created.

---
+++ super-hero-statistics/code.py
@@ -0,0 +1,23 @@
+# --------------
+#Header files
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+#Reading of the file
+data=pd.read_csv(path)
+data.replace(to_replace='-',value='Agender',inplace=True)
+print(data.head())
+data['Gender'].value_counts().plot(kind='bar')
+plt.show()
+data['Alignment'].value_counts().plot(kind='bar')
+plt.show()
+highheroes=data['Total'].quantile(q=0.99)
+newdata=data[data['Total']>554]
+super_best_names=newdata['Name'].tolist()
+print(super_best_names)
+# Code starts here
+
+
+
